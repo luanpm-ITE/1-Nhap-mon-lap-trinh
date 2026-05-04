04bc3998dc0805f611933ad56bcb74671ee79c6f
Pham Minh Luan <24730110@ms.uit.edu.vn>
2026-05-04 07:34:54 +0700

    hoan thanh bai tap b3

--- bai-tap-b3/1-Mang2Chieu.c
+++ bai-tap-b3/1-Mang2Chieu.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 int main()
 {
-   int choice,m,n,a[10][10];
+   int choice,m,n,a[10][10],b[10][10],c[10][10];
    int arr[3*3];
    int k=0;
    printf("\nNhap m va n: ");scanf("%d%d",&m,&n);
@@ -17,14 +17,24 @@ int main()
          a[i][j]=(rand()%201)-100;
       }
    }
+   for(int i=0;i<m;++i)
+   {
+      for(int j=0;j<n;++j)
+      {
+         b[i][j]=(rand()%201)-100;
+      }
+   }
    tt:
    do
    {
       printf("----MENU-----");
       printf("\n 1. Xuat ma tran ngau nhien");
       printf("\n 2. Tim min max");
-      printf("\n 3. Sap xep tang/ngich dao");
+      printf("\n 3. Sap xep tang");
       printf("\n 4. Tinh tong & tich cua ma tran");
+      printf("\n 5. Tinh tong & tich cua 2 ma tran");
+      printf("\n 6. Dem duong");
+      printf("\n 7. Dem chan");
       printf("\n Chon: ");
       scanf("%d",&choice);
    } while (0);
@@ -115,7 +125,90 @@ int main()
          printf("\n");
       }
       break;
-   default: printf("\nNhap tu 1-->5 thoi nhe!");
+   case 4:
+      int tong=0;
+      long long tich=0;
+      printf("Ma tran ngau nhien:\n");
+      for(int i=0;i<m;++i)
+      {
+         for(int j=0;j<n;++j)
+         {
+            printf("%d\t",a[i][j]);
+            tong+=a[i][j];
+            tich*=a[i][j];
+         }
+         printf("\n");
+      }
+      printf("\nTong: %d",tong);
+      printf("\nTich: %lld",tich);
+      break;
+   case 5:
+      printf("Ma tran ngau nhien A:\n");
+      for(int i=0;i<m;++i)
+      {
+         for(int j=0;j<n;++j)
+            printf("%d\t",a[i][j]);
+         printf("\n");
+      }
+      printf("Ma tran ngau nhien B:\n");
+      for(int i=0;i<m;++i)
+      {
+         for(int j=0;j<n;++j)
+            printf("%d\t",b[i][j]);
+         printf("\n");
+      }
+      //Thuat toan tong 2 ma tran
+      for(int i=0;i<m;++i)
+      {
+         for(int j=0;j<n;++j)
+            c[i][j]=a[i][j]+b[i][j];
+      }
+      for(int i=0;i<m;++i)
+      {
+         for(int j=0;j<n;++j)
+            c[i][j]=a[i][j]+b[i][j];
+      }
+      printf("Ma tran tong C:\n");
+      for(int i=0;i<m;++i)
+      {
+         for(int j=0;j<n;++j)
+            printf("%d\t",c[i][j]);
+         printf("\n");
+      }
+      break;
+   case 6:
+      printf("Ma tran ngau nhien:\n");
+      for(int i=0;i<m;++i)
+      {
+         for(int j=0;j<n;++j)
+            printf("%d\t",a[i][j]);
+         printf("\n");
+      }
+      int count=0;
+      for(int i=0;i<m;++i)
+      {
+         for(int j=0;j<n;++j)
+            if(a[i][j]>0) ++count;  
+      }
+      printf("\nSo phan tu duong: %d",count);
+      break;
+   case 7:
+      printf("Ma tran ngau nhien:\n");
+      for(int i=0;i<m;++i)
+      {
+         for(int j=0;j<n;++j)
+            printf("%d\t",a[i][j]);
+         printf("\n");
+      }
+      int chan=0;
+      for(int i=0;i<m;++i)
+      {
+         for(int j=0;j<n;++j)
+            if(a[i][j]%2==0) ++chan;  
+      }
+      printf("\nSo phan tu chan: %d",chan);
+      break;
+   default: printf("\nNhap tu 1-->7 thoi nhe!");
       break;
    }
    printf("\nCo tiep tuc khong?c/K? ");
