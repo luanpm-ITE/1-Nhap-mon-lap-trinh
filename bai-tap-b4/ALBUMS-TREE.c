5d2fdce87cebed0d733629e96b2714384879a076
Pham Minh Luan <24730110@ms.uit.edu.vn>
2026-06-05 21:30:36 +0700

    update album-tree=crud final

--- bai-tap-b4/ALBUMS-TREE.c
+++ bai-tap-b4/ALBUMS-TREE.c
@@ -34,21 +34,21 @@ Album* scanfAlbum()
 {
    Album *al=(Album*)malloc(sizeof(Album));
    printf("\nNhap thong tin Album:");
-   printf("\nNhap id: ");
+   printf("\nEnter id: ");
    scanf("%d",&al->id);
    getchar();
 
-   printf("Nhap title: ");
+   printf("nEnter title: ");
    fgets(al->title,sizeof(al->title),stdin);
-   al->title[strcspn(al->title, "\n")] = '\0';
+   al->title[strcspn(al->title,"\n")]='\0';
 
-   printf("Nhap url: ");
+   printf("nEnter url: ");
    fgets(al->url,sizeof(al->url),stdin);
-   al->url[strcspn(al->url, "\n")] = '\0';
+   al->url[strcspn(al->url,"\n")]='\0';
 
-   printf("Nhap thumbnailUrl: ");
+   printf("nEnter thumbnailUrl: ");
    fgets(al->thumbnailUrl,sizeof(al->thumbnailUrl),stdin);
-   al->thumbnailUrl[strcspn(al->thumbnailUrl, "\n")] = '\0';
+   al->thumbnailUrl[strcspn(al->thumbnailUrl,"\n")]='\0';
    
    return al;
 }
@@ -104,12 +104,78 @@ TNode* searchAlbumByID(TREE t,int id)
    if(t->key.id>id) return searchAlbumByID(t->left,id);
    return searchAlbumByID(t->right,id);
 }
-
-
+TNode* updateTitleAlbumByID(TREE t,int id)
+{
+   if(t==NULL) return NULL;
+   if(t->key.id==id)
+   {
+      printf("\nEnter new title: ");
+      scanf("%s",&t->key.title);
+      return t;
+   }
+   if(t->key.id>id) return updateTitleAlbumByID(t->left,id);
+   return updateTitleAlbumByID(t->right,id);
+}
+TNode* findMin(TNode* t) {
+   while(t->left!=NULL) 
+      t=t->left;
+   return t;
+}
+void deleteAlbumByID(TREE *t,int id) 
+{
+   if(*t==NULL) return;
+   if(id<(*t)->key.id) 
+      deleteAlbumByID(&(*t)->left,id);
+   else if(id>(*t)->key.id) 
+      deleteAlbumByID(&(*t)->right,id);
+   else 
+   {
+      // tìm thấy node cần xóa
+      if((*t)->left==NULL&&(*t)->right==NULL) 
+      {
+         free(*t);
+         *t=NULL;
+      }
+      else if((*t)->left==NULL) 
+      {
+         TNode* temp=(*t)->right;
+         free(*t);
+         *t=temp;
+      }
+      else if((*t)->right==NULL) 
+      {
+         TNode* temp=(*t)->left;
+         free(*t);
+         *t=temp;
+      }
+      else
+      {
+         // có 2 con
+         TNode* temp=findMin((*t)->right);
+         (*t)->key=temp->key;
+         deleteAlbumByID(&(*t)->right, temp->key.id);
+      }
+   }
+}
+void destroyTree(TREE *t) 
+{
+   if (*t == NULL) return;
+   destroyTree(&(*t)->left);
+   destroyTree(&(*t)->right);
+   free(*t);
+   *t = NULL; 
+}
+TNode* postAlbum(TREE *t) 
+{
+   Album *al=createAlbum(6,"kidbuu","NULL","NULL");   
+   TNode *p=createTNode(*al);
+   free(al);                   
+   return insertTNode(t,p);    
+}
 int main()
 {
    //printf("HelloWorld!");
-   int n=5;int id;
+   int n=5,id,choice;
    Album albums[MAX_ALBUM] = 
       {
       {1, "accusamus beatae ad facilis cum similique",
@@ -128,30 +194,74 @@ int main()
          "https://via.placeholder.com/600/f66b97",
          "https://via.placeholder.com/150/f66b97"}
       };
-   TREE t;TNode *p;
+   TREE t;TNode *p;TREE tNew;
    createTree(&t);
    for(int i=0;i<n;++i)
    {
       p=createTNode(albums[i]);
       insertTNode(&t,p);
    }
-   printf("\nMy ALbum: ");
-   LRN(t);
-   printf("\nNhap id: ");
-   scanf("%d",&id);
-   TREE tSearch;
-   tSearch=searchAlbumByID(t,id);
-   if(tSearch!=NULL)
+   tt:
+         printf("\n===========================");
+         printf("\n       MENU-ALBUM-CRUD      ");
+         printf("\n===========================");
+         printf("\n 1. GET ALL ALBUM.");
+         printf("\n 2. GET ALBUM BY ID.");
+         printf("\n 3. POST ALBUM BY ID.");
+         printf("\n 4. UPDATE ALBUM BY ID.");
+         printf("\n 5. DELETE ALBUM BY ID.");
+         printf("\n 6. DESTROY ALBUM.");
+         printf("\nChoice: ");scanf("%d",&choice);
+   switch (choice)
    {
-      printf("\nResult: %d",id);
-      printfAlbum(tSearch->key);
+   case 1:
+      printf("\nMY ALBUM.");
+      LRN(t);
+      break;
+   case 2:
+      printf("\nEnter ID: ");
+      scanf("%d",&id);
+      tNew=searchAlbumByID(t,id);
+      if(tNew!=NULL)
+      {
+         printf("\nResult: %d",id);
+         printfAlbum(tNew->key);
+      }
+      else printf("\nNot fount");
+      break;
+   case 3:
+      tNew=postAlbum(&t);
+      if(tNew==NULL) 
+         printf("\nAlbum with this ID already exists!");
+      else
+      {
+         printf("\nAlbum added successfully:");
+         printfAlbum(tNew->key);
+      } 
+      break;
+   case 4:
+      printf("\nEnter ID: ");
+      scanf("%d",&id);
+      tNew=updateTitleAlbumByID(t,id);
+      printf("\nAlbum ID %d after updated:",id);
+      printfAlbum(tNew->key);
+      break;
+   case 5:
+      printf("\nEnter ID: ");
+      scanf("%d",&id);
+      deleteAlbumByID(&t,id);
+      printf("\nAlbum after delete:");
+      LRN(t);
+      break;
+   case 6:
+      destroyTree(&t);
+      printf("\nTree destroyed. All albums deleted.\n");
+      break;
+   default:printf("\nEnter 1->6!!!");
+      break;
    }
-   else printf("\nKhong tim duoc Album ID %d",id);
-   
-
-
-
-
+   printf("\nDo you want continue?y/N? ");
+   if(getch()=='y'||getch()=='Y')goto tt;
    return 0;
 }
 
